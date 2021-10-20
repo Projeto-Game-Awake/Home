3f59f4547880db5d9f30c1fa0aa0e93e01343905
Game-Awake <projeto.game.awake@gmail.com>
2021-10-19 21:48:58 -0300

    alfa 0.0.5
    Adicionar verificação mobile

--- js/GameAwakeUtils.js
+++ js/GameAwakeUtils.js
@@ -1,1 +1,39 @@
 var GameAwakeUtils = {};
+
+function isMobile ()
+{
+	'use strict';
+	
+	let device = false;
+	
+	function checkUserAgent (name)
+	{
+		if (navigator.userAgent.indexOf(name) != -1)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	if (checkUserAgent('Android'))
+	{
+		device	= true;
+	}
+	else if (checkUserAgent('iPhone') && !window.MSStream)
+	{
+		device	= true;
+	}
+	else if (checkUserAgent('Mac OS') || checkUserAgent('Macintosh'))
+	{
+		device = false;
+	}
+	else if (checkUserAgent('Windows'))
+	{
+		device = false;
+	}
+
+	return device;
+}
